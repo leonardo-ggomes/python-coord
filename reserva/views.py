71b45c564b67f1376b897978684d347864969191
Leonardo Garcia <leonardoggarcia48@gmail>
2023-10-07 18:39:41 -0300

    Fix context on delete

--- reserva/views.py
+++ reserva/views.py
@@ -7,10 +7,42 @@ import json
 def eventos(request):    
  
     eventos = Evento.objects.select_related('turma','docente','ambiente').all()
-    
-    data = []
-    
+    msg = "Atualizado!"
+    data = []        
     forms = EventoForm()
+    context = None
+        
+    if request.method == 'POST':
+            
+        forms = EventoForm(request.POST)
+            
+        if forms.is_valid():                      
+      
+            if not Evento.objects.filter(
+                    ambiente_id=forms.data['ambiente'],
+                    diasemana=forms.data['diasemana'],
+                    inicio__lt=forms.data['fim'],
+                    fim__gt=forms.data['inicio']
+                ).exclude(inicio=forms.data['fim']).exists():                   
+                   forms.save()
+                   msg = "Salvo com sucesso!"
+                   forms = EventoForm()
+                   eventos = Evento.objects.select_related('turma','docente','ambiente').all()
+            else:
+                msg = "Já existe reserva neste período"
+                            
+        else:
+            forms = EventoForm() 
+    elif  request.method == 'DELETE': 
+        
+        try:
+            paramId = request.GET.get("id")              
+            item = Evento.objects.filter(id=paramId)            
+            item.delete()          
+            return HttpResponse(status=200)
+        except:        
+            return HttpResponse(status=500)
+    
     
     for item in eventos:
         data.append({
@@ -28,46 +60,8 @@ def eventos(request):
     context = {       
         'form': forms,
         'eventos':  json.dumps(data),
-        'msg':''
-    }
-    
-    if request.method == 'POST':
-            
-        forms = EventoForm(request.POST)
-            
-        if forms.is_valid():                      
-      
-            if not Evento.objects.filter(
-                    ambiente_id=forms.data['ambiente'],
-                    diasemana=forms.data['diasemana'],
-                    inicio__lt=forms.data['fim'],
-                    fim__gt=forms.data['inicio']
-                ).exclude(inicio=forms.data['fim']).exists():                   
-                   forms.save()
-                   context['msg'] = "Salvo com sucesso!"
-                   eventos = Evento.objects.select_related('turma','docente','ambiente').all()
-            else:
-                context['msg'] = "Já existe reserva neste período"
-         
-            #return render(request, "painel.html", context)
-            #return HttpResponseRedirect("/painel/")          
-          
-            
-        else:
-            forms = EventoForm() 
-    elif  request.method == 'DELETE': 
-        
-        try:
-            paramId = request.GET.get("id")              
-            item = Evento.objects.filter(id=paramId)            
-            item.delete()
-            context['msg'] = "Excluído com sucesso!"
-            return HttpResponse(status=200)
-        except:
-            context['msg'] = "Erro 500!"
-            return HttpResponse(status=500)
-        
-             
+        'msg': msg 
+    } 
    
     
     return render(request, "painel.html", context)
